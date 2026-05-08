1c01121528ba7b68d98df1c91902f1320f3f3740
dhwalief <dhiwa.bakar@gmail.com>
2026-05-09 02:30:36 +0800

    update base URL for ChatOpenAI to correct endpoint

--- app/test1.py
+++ app/test1.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
-    base_url="http://134.199.206.176:11434/v1",
+    base_url="http://134.199.206.176:8000/v1",
     api_key="dummy",
     model="qwen2.5:3b",
     temperature=0.7,
